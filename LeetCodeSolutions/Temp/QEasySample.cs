1f952afee1670374815c2ce6016ac99f2c7343dc
Febin Joy <febinjoy@gmail.com>
2024-01-01 17:32:53 +1100

    Add a sample question class

---
+++ LeetCodeSolutions/Temp/QEasySample.cs
@@ -0,0 +1,27 @@
+using LeetCodeSolutions.Enums;
+using LeetCodeSolutions.Interfaces;
+
+namespace LeetCodeSolutions.Questions.Easy
+{
+    internal class QEasySample : IQuestion
+    {
+        public int QuestionNumber { get; private set; }
+        public string QuestionText { get; private set; }
+        public string QuestionLink { get; private set; }
+        public Difficulty Difficulty { get; private set; }
+
+        public QEasySample()
+        {
+            QuestionNumber = 0;
+            QuestionText = "Question";
+            QuestionLink = "Link";
+            Difficulty = Difficulty.Easy;
+        }
+
+        public IResult Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
