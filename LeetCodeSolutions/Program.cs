f6ee3282bd9d877ecd0f27b7fed1cfbffa96cb86
Febin Joy <febinjoy@gmail.com>
2024-01-02 10:51:56 +1100

    Create QuestionService

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -4,52 +4,10 @@ using LeetCodeSolutions.Interfaces;
 
 namespace LeetCodeSolutions
 {
-    class Program
+    internal class Program
     {
-        private static readonly IQuestionFactory _questionFactory = new QuestionFactory();
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            // Easy
-            ExecuteQuestion(2331);
-            ExecuteQuestion(58);
-            ExecuteQuestion(26);
-            ExecuteQuestion(9);
-            ExecuteQuestion(35);
-            ExecuteQuestion(2974);
-
-            // Medium
-            ExecuteQuestion(2);
-
-            // Hard
-
-        }
-
-        private static void ExecuteQuestion(int questionNumber)
-        {
-            try
-            {
-                IQuestion question = _questionFactory.GetQuestionByNumber(questionNumber);
-                IResult result = question.Execute();
-                if (result.Status == Enums.ResultStatus.Passed)
-                {
-                    var message = string.IsNullOrWhiteSpace(result.Message) ? $"Question {question.QuestionNumber}. '{question.QuestionText}' passed." : result.Message;
-                    Console.WriteLine(message);
-                }
-                else
-                {
-                    Console.WriteLine($"Question {question.QuestionNumber}. '{question.QuestionText}' failed with message: {result.Message}.");
-                }
-            }
-            catch (QuestionNotFoundException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (NotImplementedException)
-            {
-                Console.WriteLine($"Question {questionNumber} is not implemented.");
-            }
         }
     }
 }
-
---
+++ LeetCodeSolutions/QuestionService.cs
@@ -0,0 +1,45 @@
+﻿using LeetCodeSolutions.Classes;
+using LeetCodeSolutions.Exceptions;
+using LeetCodeSolutions.Interfaces;
+
+namespace LeetCodeSolutions
+{
+    public class QuestionService
+    {
+        private readonly IQuestionFactory _questionFactory;
+
+        public QuestionService()
+        {
+            _questionFactory = new QuestionFactory();
+        }
+
+        public IResult ExecuteQuestion(int questionNumber)
+        {
+            try
+            {
+                IQuestion question = _questionFactory.GetQuestionByNumber(questionNumber);
+                IResult result = question.Execute();
+                if (result.Status == Enums.ResultStatus.Passed)
+                {
+                    var message = string.IsNullOrWhiteSpace(result.Message) ? $"Question {question.QuestionNumber}. '{question.QuestionText}' passed." : result.Message;
+                    Console.WriteLine(message);
+                }
+                else
+                {
+                    Console.WriteLine($"Question {question.QuestionNumber}. '{question.QuestionText}' failed with message: {result.Message}.");
+                }
+                return result;
+            }
+            catch (QuestionNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            catch (NotImplementedException)
+            {
+                Console.WriteLine($"Question {questionNumber} is not implemented.");
+                throw;
+            }
+        }
+    }
+}
