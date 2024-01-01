55e0dcbdfc13305c91dc2b57baec1be8d2467191
Febin Joy <febinjoy@gmail.com>
2024-01-02 10:51:07 +1100

    Add Test Project

---
+++ LeetCodeSolutionsTest/QuestionServiceTests.cs
@@ -0,0 +1,18 @@
+using LeetCodeSolutions;
+
+namespace LeetCodeSolutionsTest
+{
+    public class QuestionServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
